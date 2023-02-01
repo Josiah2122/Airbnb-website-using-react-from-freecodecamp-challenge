6508d000b4e19728ed01005a4db0a8761e22d0ec
Josiah2122 <98037818+Josiah2122@users.noreply.github.com>
2023-02-01 12:30:44 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from './components/data'
+
+import  "./index.css"
+
+
+function Page() {
+ const cardElement = data.map(data => {
+  return(
+   <Card 
+    key={data.id}
+    image={data.coverImg}
+    rating={data.stats.rating}
+    reviewCount={data.stats.reviewCount}
+    country={data.location}
+    title={data.title}
+    price={data.price}
+    status={data.status}
+   />
+  ) 
+ })
+ return(
+  <div>
+   <NavBar />
+   <Hero />
+   <div className="card-container">
+   {cardElement}
+   </div>
+  </div>
+ )
+}
+
+
+
+ReactDOM.render(<Page />, document.getElementById('root'))
