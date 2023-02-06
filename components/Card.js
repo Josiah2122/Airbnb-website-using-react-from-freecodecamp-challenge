c06ab36531501d1ec9b1ea249fad98ad493f99ce
Josiah2122 <98037818+Josiah2122@users.noreply.github.com>
2023-02-06 09:51:35 +0300

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,15 +1,14 @@
 import React from "react"
 
 export default function Card(props){
- return(
- 
-    <div className="card">
-     <img src={`./images/${props.image}`} />
-     { props.status &&<button>{props.status}</button>}
-     <div className="rating"> <img src="./images/star.png" /> <p>{props.rating} <span>({props.reviewCount})<strong> ·</strong> {props.country}</span></p></div>
-     <p>{props.title}</p>
-     <p><strong>From ${props.price} </strong>/ person</p>
-    </div>
+return(
+   <div className="card">
+   <img src={`./images/${props.coverImg}`} />
+   { props.status &&<button>{props.status}</button>}
+   <div className="rating"> <img src="./images/star.png" /> <p>{props.stats.rating} <span>({props.stats.reviewCount})<strong> ·</strong> {props.location}</span></p></div>
+   <p>{props.title}</p>
+   <p><strong>From ${props.price} </strong>/ person</p>
+   </div>
 
- )
+)
 }
